bff8f68f9156def226df06c5f8cb7905012e993a
guhj <hyejiku98@gmail.com>
2023-08-25 18:49:24 +0900

    [ADD] 항공정보 API 데이터 DB insert

---
+++ src/main/java/com/asianaidt/dutyfree/domain/member/domain/Flight.java
@@ -0,0 +1,29 @@
+package com.asianaidt.dutyfree.domain.member.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "flightId")
+    private Long id;
+    private String flightDate;
+    private String airlineEng;
+    private String airlineKor;
+    private String boardingEng;
+    private String boardingKor;
+    private String city;
+    private String flightCode;
+    private String flightStatus;
+
+}
--- src/main/java/com/asianaidt/dutyfree/domain/member/service/MemberService.java
+++ src/main/java/com/asianaidt/dutyfree/domain/member/service/MemberService.java
@@ -1,19 +1,33 @@
 package com.asianaidt.dutyfree.domain.member.service;
 
+import com.asianaidt.dutyfree.domain.member.domain.Flight;
 import com.asianaidt.dutyfree.domain.member.domain.Member;
 import com.asianaidt.dutyfree.domain.member.dto.MemberRequestDto;
+import com.asianaidt.dutyfree.domain.member.repository.FlightRepository;
 import com.asianaidt.dutyfree.domain.member.rerpository.MemberRepository;
 import com.asianaidt.dutyfree.global.error.StandardException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MemberService {
     private final MemberRepository memberRepository;
-
+    private final FlightRepository flightRepository;
 
     public boolean login(String id, String password){
 //        Member member = Member.builder()
@@ -50,6 +64,82 @@ public class MemberService {
         Optional<Member> member2 = memberRepository.findById(member.getId());
         if(member2.isPresent()) return false;
         memberRepository.save(member);
+
         return true;
     }
+
+    public void addFlightInfo() {
+        for (int no = 1; no <= 10; no++) {
+            try {
+                StringBuilder urlBuilder = new StringBuilder("http://openapi.airport.co.kr/service/rest/FlightStatusList/getFlightStatusList"); /*URL*/
+                String serviceKey = "SGv0JWXsrgZ3gYFyfLFe%2FfWKwsz0UU9Yr3MWrW7YZASyFZkaWRpGZElEHPwaCWobrghM4a1JmCyer60D5am%2BgQ%3D%3D";
+
+                urlBuilder.append("?").append(URLEncoder.encode("serviceKey", StandardCharsets.UTF_8)).append("=").append(serviceKey)
+                        .append("&").append(URLEncoder.encode("pageNo", StandardCharsets.UTF_8)).append("=").append(no); /*Service Key*/
+                URL url = new URL(urlBuilder.toString());
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Content-type", "application/json");
+                System.out.println("Response code: " + conn.getResponseCode());
+
+                BufferedReader rd;
+                if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                    rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                } else {
+                    rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                }
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                rd.close();
+                conn.disconnect();
+                String output = "";
+
+                try {
+                    JSONObject jObject = XML.toJSONObject(sb.toString());
+                    JSONObject itemObject = (JSONObject) ((JSONObject) jObject.get("response")).get("body");
+                    JSONArray jsonArray = (JSONArray) ((JSONObject) itemObject.get("items")).get("item");
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject object = (JSONObject) jsonArray.get(i);
+                        String flightDate = (String) object.get("flightDate");
+                        String airFln = (String) object.get("airFln");
+                        String boardingEng = (String) object.get("boardingEng");
+                        String rmkEng = (String) object.get("rmkEng");
+                        String boardingKor = (String) object.get("boardingKor");
+                        String airlineEng = (String) object.get("airlineEnglish");
+                        String airlineKor = (String) object.get("airlineKorean");
+                        String city = (String) object.get("city");
+
+                        Flight flight = Flight.builder()
+                                .flightDate(flightDate)
+                                .boardingEng(boardingEng)
+                                .boardingKor(boardingKor)
+                                .flightCode(airFln)
+                                .airlineEng(airlineEng)
+                                .airlineKor(airlineKor)
+                                .flightStatus(rmkEng)
+                                .city(city)
+                                .build();
+
+                        flightRepository.save(flight);
+                    }
+
+                } catch (Exception e) {
+                    log.info(e.getMessage());
+                }
+
+
+            } catch (IOException e) {
+                throw new StandardException("API에 연결할 수 없습니다.");
+            }
+        }
+
+    }
+
 }
